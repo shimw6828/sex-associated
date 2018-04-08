64ee1914b9ac0bb2a8c92462773aa754684cc68e
shimw6828 <shimw6828@qq.com>
2018-04-08 19:52:24 +0800

    solve the druginfo's bug

--- sadb/tool/druginfo.py
+++ sadb/tool/druginfo.py
@@ -1,4 +1,5 @@
 import urllib,urllib2
+import pandas as pd
 import sys,pymongo
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
 db = client.sadb
@@ -6,28 +7,31 @@ db.authenticate('sadb_admin','123456789',mechanism='SCRAM-SHA-1')
 url = 'http://www.uniprot.org/uploadlists/'
 
 def main():
-    with open("/opt/shimw/github/sex-associated/sadb/static/file/uniprot links.csv","r") as drugs:
-        header=drugs.readline().strip().split(",")
-        header=["DrugBank_ID","Drug_name","Type","UniProt ID","UniProt_Name","ensembl_gene_id"]
-        for drug in drugs:
-            fields = drug.rstrip("\n").split(",")
-            params = {
-                'from': 'ACC',
-                'to': 'ENSEMBL_ID',
-                'format': 'tab',
-                'query': fields[3]
-            }
-            data = urllib.urlencode(params)
-            request = urllib2.Request(url, data)
-            contact = ""  # Please set your email address here to help us debug in case of problems.
-            request.add_header('User-Agent', 'Python %s' % contact)
-            response = urllib2.urlopen(request)
-            page = response.read()
-            if len(page.split("\n"))>3:
-                print(drug)
-            ensembl_id=page.split("\n")[1].split("\t")[1]
-            fields.append(ensembl_id)
-            record = dict(zip(header, fields))
+    druglist=pd.read_csv("/opt/shimw/github/sex-associated/sadb/static/file/uniprot links.csv")
+    header=["DrugBank_ID","Drug_name","Type","UniProt ID","UniProt_Name","ensembl_gene_id"]
+    for i in druglist.index:
+        fields = druglist.loc[i]
+        params = {
+            'from': 'ACC',
+            'to': 'ENSEMBL_ID',
+            'format': 'tab',
+            'query': fields["UniProt ID"]
+        }
+        data = urllib.urlencode(params)
+        request = urllib2.Request(url, data)
+        contact = ""  # Please set your email address here to help us debug in case of problems.
+        request.add_header('User-Agent', 'Python %s' % contact)
+        response = urllib2.urlopen(request)
+        page = response.read()
+        if len(page.split("\n"))>3:
+            print(fields["UniProt ID"]+"\tmany")
+        if len(page.split("\n"))<3:
+            print(fields["UniProt ID"]+"\tzero")
+        for lists in page.split("\n")[1:-1]:
+            temp=list(fields.values)
+            ensembl_id = lists.split("\t")[1]
+            temp.append(ensembl_id)
+            record = dict(zip(header, temp))
             db.drug_info.insert_one(record)
 
 if __name__ =="__main__":
